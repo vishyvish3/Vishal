3a472093b77e753a2d6c78101951561f083aec51
vishyvish3 <67889643+vishyvish3@users.noreply.github.com>
2020-10-11 18:40:55 +0530

    Update script.js

--- Week 6/Day 1/script.js
+++ Week 6/Day 1/script.js
@@ -1,15 +1,17 @@
-var ages = [100, 33, 12, 40, 45];
-var chunk = function (ages) {
-    var split = 2;
+"use strict";
+var ages = [100, 33, 12, 40, 45, 33, 20];
+//chunk function
+var chunk = function (arr, size) {
     var outarr = [];
-    for (var i = 0; i < ages.length; i += split) {
-        outarr.push(ages.slice(i, i + split));
+    for (var i = 0; i < ages.length; i += size) {
+        outarr.push(ages.slice(i, i + size));
     }
     return outarr;
 };
-var chunk_result = chunk(ages);
-console.log("Chunk function Result : " + chunk_result);
-//sum
+var chunk_result = chunk(ages, 2);
+console.log("Chunk result: ");
+console.log(chunk_result);
+//sum function
 var sum = function (ages) {
     var sum_iteration = 0;
     for (var _i = 0, ages_1 = ages; _i < ages_1.length; _i++) {
@@ -20,36 +22,46 @@ var sum = function (ages) {
 };
 var sum_result_value = sum(ages);
 console.log("Sum function Result : " + sum_result_value);
-//filter
-var filter = function (ages, filer_input) {
+//filter function
+var filter = function (ages, filter) {
     var res_arr = [];
     for (var _i = 0, ages_2 = ages; _i < ages_2.length; _i++) {
         var val = ages_2[_i];
-        if (val > filer_input) {
+        if (val == filter) {
             res_arr.push(val);
         }
     }
-    return res_arr;
+    if (res_arr.length > 0) {
+        return res_arr;
+    }
+    else {
+        return 'not found';
+    }
 };
-var filer_input = 20;
-var filter_result = filter(ages, filer_input);
+var filter_input = 1000;
+var filter_result = filter(ages, filter_input);
 console.log("Filter function result : " + filter_result);
-//find
+//find function
 var find = function (ages, input) {
-    var res;
+    var res = 0;
     for (var _i = 0, ages_3 = ages; _i < ages_3.length; _i++) {
         var val = ages_3[_i];
-        if (val > input) {
+        if (val == input) {
             res = val;
             break;
         }
     }
-    return res;
+    if (res) {
+        return res;
+    }
+    else {
+        return 'not found';
+    }
 };
 var input = 20;
 var find_result_value = find(ages, input);
 console.log("Find function result : " + find_result_value);
-//reduce
+//reduce function
 var reduce = function (ages) {
     var sub_iteration = 100;
     for (var _i = 0, ages_4 = ages; _i < ages_4.length; _i++) {
